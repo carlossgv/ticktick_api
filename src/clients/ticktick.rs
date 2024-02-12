2be6fb43ace4ac5ab6ff3ffeba8ae6ae8ff35460
Carlos Gonzalez <carlossgv@gmail.com>
2024-02-12 13:04:26 -0300

    feat: extract file path to function

--- src/clients/ticktick.rs
+++ src/clients/ticktick.rs
@@ -1,8 +1,19 @@
+use dirs::home_dir;
 use reqwest;
 use serde::{Deserialize, Serialize};
+use std::fs;
 
 const TICK_TICK_URL: &str = "https://api.ticktick.com/api/v2";
 const X_DEVICE_HEADER: &str = r#"{"platform":"web","os":"macOS 10.15.7","device":"Chrome 121.0.0.0","name":"","version":5070,"id":"65bcdf6491ea1a2e7db71fbe","channel":"website","campaign":"","websocket":""}"#;
+const DATA_FILE: &str = ".ticktick_data";
+
+fn get_file_path(filename: &str) -> String {
+    if let Some(home_dir) = home_dir() {
+        return home_dir.join(filename).to_string_lossy().to_string();
+    } else {
+        panic!("Unable to determine the home directory.");
+    }
+}
 
 #[derive(Debug, Serialize, Deserialize)]
 struct LoginBody {
@@ -52,13 +63,54 @@ pub async fn login(username: &str, password: &str) -> Result<(), Box<dyn std::er
 
     match response.status().is_success() {
         true => {
-            let login_response: LoginResponse = response.json().await?;
-            println!("Token: {}", login_response.token);
+            let absolute_file_path: String = get_file_path(DATA_FILE);
+            fs::write(&absolute_file_path, "")?;
+
+            let mut session_cookies = Vec::<String>::new();
+            for cookie in response.cookies() {
+                println!("\n Cookie: {:?}", cookie);
+                if cookie.value() == "" {
+                    println!("Cookie is empty");
+                    continue;
+                }
+                session_cookies.push(format!("{}={}", cookie.name(), cookie.value()));
+            }
+
+            fs::write(absolute_file_path, session_cookies.join(";"))?;
         }
         false => {
             let error_response: ErrorLoginResponse = response.json().await?;
             println!(
                 "Login failed! Error Code: {}, Message: {}, ID: {}",
+                error_response.error_code, error_response.error_message, error_response.error_id
+            );
+            println!("Remainder times: {}", error_response.data.remainder_times);
+        }
+    }
+
+    Ok(())
+}
+
+pub async fn get_user_info(token: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let client = reqwest::Client::new();
+
+    let response = client
+        .get(format!("{}/batch/check/0", TICK_TICK_URL))
+        .header("content-type", "application/json")
+        .header("x-device", X_DEVICE_HEADER)
+        .header("Authorization", format!("Bearer {}", token))
+        .send()
+        .await?;
+
+    match response.status().is_success() {
+        true => {
+            let user_info: serde_json::Value = response.json().await?;
+            println!("User Info: {}", user_info);
+        }
+        false => {
+            let error_response: ErrorLoginResponse = response.json().await?;
+            println!(
+                "Get user info failed! Error Code: {}, Message: {}, ID: {}",
                 error_response.error_code, error_response.error_message, error_response.error_id
             );
             println!("Remainder times: {}", error_response.data.remainder_times);
